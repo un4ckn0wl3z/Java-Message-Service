84b240dc788fe4e3db2031da8544b4b5067dbf53
un4ckn0wl3z <slickerth@gmail.com>
2019-09-28 18:09:22 +0700

    add header exchange and heade consumer

--- rabbitmq/src/main/java/rocks/anuwat/www/rabbitmq/RabbitmqConsumer.java
+++ rabbitmq/src/main/java/rocks/anuwat/www/rabbitmq/RabbitmqConsumer.java
@@ -1,5 +1,10 @@
 package rocks.anuwat.www.rabbitmq;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +12,19 @@ import rocks.anuwat.www.rabbitmq.model.Person;
 
 @Service
 public class RabbitmqConsumer {
-	
+
+//	@RabbitListener(queues = Constant.MOBILE_QUEUE)
+//	public void getMessage(Person person) {
+//		System.out.println(person.toString());
+//	}
+
 	@RabbitListener(queues = Constant.MOBILE_QUEUE)
-	public void getMessage(Person person) {
+	public void getMessage(byte[] message) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(message);
+		ObjectInput in = new ObjectInputStream(bis);
+		Person person =  (Person) in.readObject();
+		in.close();
+		bis.close();
 		System.out.println(person.toString());
 	}
-	
 }
--- rabbitmq/src/main/java/rocks/anuwat/www/rabbitmq/controller/TestController.java
+++ rabbitmq/src/main/java/rocks/anuwat/www/rabbitmq/controller/TestController.java
@@ -1,5 +1,12 @@
 package rocks.anuwat.www.rabbitmq.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +24,31 @@ public class TestController {
 	@Autowired
 	RabbitTemplate rabbitTemplate;
 
-	@GetMapping("/test/{name}")
-	public String testApi(@PathVariable("name") String name) {
-		Person person = new Person(1L, name);
-		rabbitTemplate.convertAndSend(Constant.MOBILE_QUEUE, person);
-		rabbitTemplate.convertAndSend(Constant.DIRECT_EXCHANGE, Constant.MOBILE_RTK, person);
-		rabbitTemplate.convertAndSend(Constant.FANOUT_EXCHANGE, "", person);
-		rabbitTemplate.convertAndSend(Constant.TOPIC_EXCHANGE, "tv.mobile.ac", person);
+//	@GetMapping("/test/{name}")
+//	public String testApi(@PathVariable("name") String name) {
+//		Person person = new Person(1L, name);
+//		rabbitTemplate.convertAndSend(Constant.MOBILE_QUEUE, person);
+//		rabbitTemplate.convertAndSend(Constant.DIRECT_EXCHANGE, Constant.MOBILE_RTK, person);
+//		rabbitTemplate.convertAndSend(Constant.FANOUT_EXCHANGE, "", person);
+//		rabbitTemplate.convertAndSend(Constant.TOPIC_EXCHANGE, "tv.mobile.ac", person);
+//
+//		return "Success.";
+//	}
 
+	@GetMapping("/test/{name}")
+	public String testApi(@PathVariable("name") String name) throws IOException {
+		Person person = new Person(1L, name);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = new ObjectOutputStream(bos);
+		out.writeObject(person);
+		out.flush();
+		out.close();
+		byte[] byteMessage = bos.toByteArray();
+		bos.close();
+		Message message = MessageBuilder.withBody(byteMessage)
+				.setHeader("item1", "mobile")
+				.setHeader("item2", "television").build();
+		rabbitTemplate.send(Constant.HEADERS_EXCHANGE, "", message);
 		return "Success.";
 	}
 
